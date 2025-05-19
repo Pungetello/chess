b153053618a2014df1b1f107938ab4b8c892c3bb
Pungetello <Pungetello@gmail.com>
2025-05-19 15:34:06 -0600

    Data Memory objects

--- shared/src/main/java/model/AuthData.java
+++ shared/src/main/java/model/AuthData.java
@@ -1,4 +1,22 @@
 package model;
 
 public class AuthData {
+    private String authToken;
+    private String username;
+
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
