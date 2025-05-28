73d392eeec85d5a731e3f5e82152d20a441fed71
Pungetello <Pungetello@gmail.com>
2025-05-28 13:49:45 -0600

    Test file setup, headers of functions

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,25 @@
 package client;
 
+import dataaccess.DataAccessException;
+import dataaccess.SQLAuthDAO;
+import dataaccess.SQLGameDAO;
+import dataaccess.SQLUserDAO;
 import org.junit.jupiter.api.*;
 import server.Server;
+import ui.ServerFacade;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade("Localhost:" + port);
     }
 
     @AfterAll
@@ -20,10 +27,29 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    @BeforeEach
+    public void createTables() throws DataAccessException {
+        new SQLAuthDAO();
+        new SQLGameDAO();
+        new SQLUserDAO();
+    }
+
+    @AfterEach
+    public void clearTables() throws Exception {
+        new SQLAuthDAO().clear();
+        new SQLGameDAO().clear();
+        new SQLUserDAO().clear();
+    }
 
     @Test
     public void sampleTest() {
         Assertions.assertTrue(true);
+    }
+
+    @Test
+    void register() throws Exception {
+        var authData = facade.register("player1", "password", "p1@email.com");
+        Assertions.assertTrue(authData.authToken().length() > 10);
     }
 
 }
